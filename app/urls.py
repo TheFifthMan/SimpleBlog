46455d8b3e6b4c49015bf11b3a9e51d53f1bfc98
John Wen <John.Wen@ehealth.com>
2018-10-15 17:41:22 +0800

    archives and category

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,re_path
-from .views import IndexView,DetailView
+from .views import IndexView,DetailView,ArchivesView,CategoriesView
 
 app_name = 'app'
 urlpatterns = [
     path('',IndexView.as_view(),name='index'),
-    re_path(r'^post/(?P<pk>[0-9]+$)',DetailView.as_view(),name='detail')
+    re_path(r'^post/(?P<pk>[0-9]+$)',DetailView.as_view(),name='detail'),
+    re_path(r'^archives/(?P<year>[0-9]+)/(?P<month>[0-9]{1,2})$',ArchivesView.as_view(),name='archives'),
+    re_path(r'^category/(?P<pk>[0-9]+$)',CategoriesView.as_view(),name='category'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,reverse,get_object_or_404
 from django.views.generic import ListView,View
 import markdown 
-from .models import  Post
+from .models import  Post,Category
 # Create your views here.
 class IndexView(ListView):
     model = Post
@@ -20,4 +20,14 @@ class DetailView(View):
                                       ])
         return render(request,'blog/single.html',{"post":post})
 
-    
+
+class ArchivesView(View):
+    def get(self,request,year,month):
+        posts = Post.objects.filter(created_timestamp__year=year,created_timestamp__month=month).order_by('-created_timestamp')
+        return render(request,'blog/index.html',{"posts":posts})
+
+class CategoriesView(View):
+    def get(self,request,pk):
+        category =get_object_or_404(Category,pk=pk)
+        posts = Post.objects.filter(category=category).all()
+        return render(request,'blog/index.html',{'posts':posts})
